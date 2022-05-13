9ef6d4417c69afdbf09e635f6c16ae99c7278acf
Adam <adam---ahmed@hotmail.com>
2022-05-13 17:28:11 +0100

    Initialised Repo

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+        //I am defining my string here. I used the double dash to leave a comment as it is the simplest way to do so.
+        String myString = "Hello World!";
+
+        /*
+        Here I print my string.
+        I used the star and dash to leave a comment as it allows me to leave multi-lin comments.
+        */
+        System.out.println(myString);
+    }
+}
